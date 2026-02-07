ea3579224101194e230b126235742915ad516b0c
chinny CircuitNight <jangmaleo@gmail.com>
2026-02-07 22:56:47 +0700

    server update

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,19 +1,12 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const pool = process.env.DATABASE_URL
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-    })
-  : new Pool({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+// ใช้ URL จาก .env
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // สำหรับเชื่อมต่อจาก localhost ไม่จำเป็นต้องใช้ SSL ก็ได้ครับ
+  ssl: false,
+});
 
 export { pool };
